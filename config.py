a827127ef185fb892836e2f73f424224db20cd24
Austin Zhang <zenith00dev@gmail.com>
2019-01-20 12:52:11 -0600

    Fix config

--- config.py
+++ config.py
@@ -6,46 +6,49 @@ import pickle
 import pprint
 
 class Config:
-    configs = {}
+    server_configs = {}
 
-    def __init__(self):
+    def __init__(self, botname):
+        self.name = botname
+        self.DEFAULTS = getattr(CONFIG_DEFAULT, self.name, {})
+        self.CONFIG = getattr(CONFIG, self.name)
         pass
 
     def get_token(self):
-        return CONFIG.PINBOT["TOKEN"]
+        return getattr(self.CONFIG["TOKEN"])
 
     def of(self, guild) -> dict:
-        if guild.id not in self.configs.keys():
+        if guild.id not in self.server_configs.keys():
             self.initialize_default(guild_id=guild.id)
             self.save()
-        return self.configs[guild.id]
+        return self.server_configs[guild.id]
 
     def initialize_default(self, guild_id):
-        self.configs[guild_id] = copy.deepcopy(CONFIG_DEFAULT.PINBOT)
+        self.server_configs[guild_id] = copy.deepcopy(self.DEFAULTS)
         self.save()
 
     def reset_key(self, guild_id, key):
         try:
-            self.configs[guild_id][key] = copy.deepcopy(CONFIG_DEFAULT.PINBOT[key])
+            self.server_configs[guild_id][key] = copy.deepcopy(self.DEFAULTS[key])
         except KeyError:
             print(traceback.format_exc())
-            del self.configs[guild_id][key]
+            del self.server_configs[guild_id][key]
 
     def save(self):
         with open("configs.pickle", "wb") as f:
-            pickle.dump(self.configs, f)
+            pickle.dump(self.server_configs, f)
         return self
 
     def load(self):
         try:
             with open("configs.pickle", "rb") as f:
-                self.configs = pickle.load(f)
+                self.server_configs = pickle.load(f)
         except IOError:
             print(traceback.format_exc())
         return self
 
     def generate_readable(self, guild_id = None):
         if not guild_id:
-            return pprint.pformat(self.configs)
+            return pprint.pformat(self.server_configs)
         else:
-            return pprint.pformat(self.configs[guild_id])
+            return pprint.pformat(self.server_configs[guild_id])
