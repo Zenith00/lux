972f91828293aeddcf11cd9de5442c1e25aa435b
Austin Zhang <austin.g.zhang@zoom.us>
2019-07-31 09:49:27 -0700

    update

--- client.py
+++ client.py
@@ -49,8 +49,7 @@ class Lux(discord.Client):
     async def on_message(self, message):
         ctx = Contexter(message=message, configs=self.config, auth_func=self.auth_function)
         # print(f"ctx.config: {ctx.config}, guild is {message.guild.id}")
-        if message.guild is None:
-            await message.author.send(await self.commands["help"].execute(ctx))
+
         if message and message.content and message.content.startswith(ctx.config["PREFIX"]):
             command_raw = ctx.deprefixed_content.lower()
             if command_raw in self.commands:
